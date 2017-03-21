f16da6b516fcaf9df758d4a91508f2185b01e70e
mikal <mikal.callahan@icloud.com>
2017-03-21 00:56:45 -0700

    added milligram, normalize, jquery. Created sketch for new portfolio.

--- js/menu.js
+++ js/menu.js
@@ -1,31 +1,14 @@
-/*
-$(document).ready(function() {
-    $('#toggle').click(function() {
-        $(this).toggleClass('active');
-        $('#overlay').toggleClass('open');
-    });
-});
-*/
-/*
-$(document).ready(function() {
-    $('.toggle').click(function() {
-        $('#menu').style.display = "block";
-    });
-    $('#menu').click(function() {
-        $('#menu').fadeOut();
-    });
-});
-*/
 $(document).ready(function() {
     $('#toggle').click(function() {
         $(this).toggleClass('active');
         $('#menu').toggleClass('open');
         $("#toggleicon").toggleClass("open");
         $("fa-genderless").toggleClass("open");
-        //$("#toggleicon").toggleClass("fa-circle-o");
-        $("#toggleicon").toggleClass("fa-circle");
-        //$("#toggle").toggleClass("fa fa-circle");
-        //$("#toggle").toggle();
-
+    });
+    $('#menuitems').click(function() {
+        $(this).toggleClass('active');
+        $('#menu').toggleClass('open');
+        $("#toggleicon").toggleClass("open");
+        $("fa-genderless").toggleClass("open");
     });
 });
